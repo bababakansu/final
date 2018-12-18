785d24f6950de7f0fc6b5b228e6ba5c80f907ed0
野村侑史 <nomurayushi@nomurayuushinoMacBook-puro.local>
2018-12-18 10:06:21 +0900

    first commit

---
+++ final.js
@@ -0,0 +1,30 @@
+function uranai(){
+  var num = Math.random();
+  if (num < 0.1){
+    alert("凶");
+  }else if(0.1 <= num && num < 0.33){
+    alert("小吉");
+  }else if(0.33 <= num && num < 0.66){
+    alert("中吉");
+  }else{
+    alert("大吉");
+  }
+}
+
+window.onload = function(){
+  draw();
+}
+
+function draw(){
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  var x, y;
+  for (y=0; y<100; y = y + 10){
+    for (x=0; x<100; x = x + 10){
+    if (50 <= y && (x+y) == 90){
+      ctx.fillRect(x,y,10,10);
+      }
+    }
+  }
+}
