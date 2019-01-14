52af74327a04ebdcf92424c5020eece04084cac8
野村侑史 <nomurayushi@nomurayuushinoMacBook-puro.local>
2019-01-15 05:42:18 +0900

    まだ途中

--- final.js
+++ final.js
@@ -1,29 +1,63 @@
-function uranai(){
+function color(){
   var num = Math.random();
   if (num < 0.1){
-    alert("凶");
+    alert("赤");
   }else if(0.1 <= num && num < 0.33){
-    alert("小吉");
+    alert("白");
   }else if(0.33 <= num && num < 0.66){
-    alert("中吉");
+    alert("黒");
   }else{
-    alert("大吉");
+    alert("青");
   }
 }
 
-window.onload = function(){
-  draw();
+function item(){
+  var num = Math.random();
+  if (num < 0.1){
+    alert("リュック");
+  }else if(0.1 <= num && num < 0.33){
+    alert("腕時計");
+  }else if(0.33 <= num && num < 0.66){
+    alert("ネックレス");
+  }else{
+    alert("帽子");
+  }
 }
 
-function draw(){
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
 
-  var x, y;
-  for (y=0; y<100; y = y + 10){
-    for (x=0; x<100; x = x + 10){
-    if ((x+y)%20 == 0 && x%20!=0 && y%20!=0){
-      ctx.fillRect(x,y,10,10);
+
+
+
+function question(){
+var a = document.getElementById('color');
+var b = document.getElementById('item');
+var c = document.getElementsByName('gender');
+var d = document.getElementsByName('with');
+var e = document.getElementsByName('way');
+
+var f = document.getElementById('male');
+var g = document.getElementById('female');
+
+var h =document.getElementById('boyfriend');
+var i =document.getElementById('girlfriend');
+var j =document.getElementById('family');
+var k =document.getElementById('only');
+var l =document.getElementById('nothing');
+
+var m =document.getElementById('school');
+var n =document.getElementById('date');
+var o =document.getElementById('job');
+var p =document.getElementById('sport');
+var q =document.getElementById('shop');
+var r =document.getElementById('amusement');
+var s =document.getElementById('museaum');
+var t =document.getElementById('trip');
+
+if(f == true){
+  if(h == true){
+    if(m == true || o  == true || q == true){
+      alert ("やったー")
+      document.getElemetnById('alert').innerHTML = '<img src= "img.png" alt="photo-1.jpg">';
       }
     }
   }
